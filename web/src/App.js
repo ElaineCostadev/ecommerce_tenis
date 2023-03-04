47c0b91b25d2fe156c059278b69769ee381f3084
elainecostadev <elainerscosta@hotmail.com>
2023-03-04 18:43:18 -0300

    ajustes gerais de configuracoes

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,19 @@
 import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ProductsProvider from "./context/ProductsProvider";
 import Home from "./pages/Home";
 import ShoppingCart from "./pages/ShoppingCart";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/shopping" element={ <ShoppingCart /> } />
-      </Routes>
+      <ProductsProvider>
+        <Navbar />
+          <Routes>
+            <Route path="/" element={ <Home /> } />
+            <Route path="/shopping" element={ <ShoppingCart /> } />
+          </Routes>
+      </ProductsProvider>
     </div>
   );
 }
