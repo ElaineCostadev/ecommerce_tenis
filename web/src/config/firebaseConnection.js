4c4e1a24290252de3b88a2d6a1b73f84994eab32
elainecostadev <elainerscosta@hotmail.com>
2023-03-04 18:41:44 -0300

    :fix ajustando a conexao com banco com .env

--- web/src/config/firebaseConnection.js
+++ web/src/config/firebaseConnection.js
@@ -1,18 +1,27 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
+// require('dotenv').config()
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+  REACT_APP_FIREBASE_MEASUREMENT_ID
+} = process.env
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDOnT52rOtJu1XYg4r0-ZFqQ9IrRlk4ytM",
-  authDomain: "cognydatabase.firebaseapp.com",
-  projectId: "cognydatabase",
-  storageBucket: "cognydatabase.appspot.com",
-  messagingSenderId: "675493603078",
-  appId: "1:675493603078:web:0fe767743b7efea6852087",
-  measurementId: "G-VSM8VFD9V3"
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain:REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-
 
 
 const app = initializeApp(firebaseConfig);
