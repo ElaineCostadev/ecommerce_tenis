9d8d4be77c2c6b8b10b883a89ee8329d669fd9cf
elainecostadev <elainerscosta@hotmail.com>
2023-03-04 18:42:21 -0300

    :feat: pagina da Home e do Carrinho prontas

--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -1,35 +1,53 @@
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from "../config/firebaseConnection";
-import { useEffect, useState } from 'react';
+// import { collection, getDocs } from 'firebase/firestore';
+// import { db } from "../config/firebaseConnection";
+// import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { useCart } from '../context/ProductsProvider';
+import '../styles/Products.css'
 
 function Home() {
-  const [products, setProducts] = useState([]);
-  const productsCollectionRef = collection(db, "Products")
-
-  useEffect(() => {
-    const getProducts = async () => {
-      const data = await getDocs(productsCollectionRef)
-      setProducts(data.docs.map((eachDoc) => ({ ...eachDoc.data(), id: eachDoc.id })));
-    };
-    getProducts();
-  },[]);
-
+  const { products, addProduct, message } = useCart();
+  const [quantity, setQuantity] = useState(1)
 
   return (
     <div className='products'>
+      <div className='message'>{(message && 'PRODUTO INSERIDO NO CARRINHO')}</div>
       {
         products.map((eachProduct) => {
           return (
             <div className='eachProduct' key={eachProduct.id}>
-              <img src={eachProduct.urlImage} alt={eachProduct.description}/>
-              <h2>{eachProduct.brand}</h2>
-              <h3>{eachProduct.description}</h3>
-              <h4>R$ {eachProduct.price},00</h4>
+              <img className='eachImage'src={eachProduct.urlImage} alt={eachProduct.description}/>
+              <h2 className='eachBrand'>{eachProduct.brand}</h2>
+              <h3 className='eachDescription'>{eachProduct.description}</h3>
+              <h3 className='eachPrice'>R$ {eachProduct.price},00</h3>
+              <div className='buttonAndInput'>
+                <label htmlFor="quantity">
+                  <input
+                    className='eachQuantity'
+                    type="number"
+                    id="quantity"
+                    name="quantity"
+                    value={ quantity }
+                    onChange={ (e) => setQuantity(e.target.value) }
+                  />
+                </label>
+                  
+                <button
+                  className='eachAddButton'
+                  // disabled={}
+                  type="button"
+                  onClick={ () => addProduct(eachProduct) }
+                  >
+                    Adicionar ao carrinho
+                </button>
+              </div>
+
             </div>
-          );
-        })}
+        );
+      })}
     </div>
   );
 }
 
 export default Home
+
--- web/src/pages/ShoppingCart.js
+++ web/src/pages/ShoppingCart.js
@@ -1,8 +1,101 @@
-import React from 'react'
+import { addDoc, collection } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useCart } from '../context/ProductsProvider'
+import '../styles/ShoppingCart.css'
+import { db } from "../config/firebaseConnection";
 
 function ShoppingCart() {
+  const { cart, setCart, removeProduct, message, setMessage} = useCart();
+  const [cartTotalPrice, setCartTotalPrice] = useState(0);
+  const navegate = useNavigate();
+
+  const shoppingCartRef = collection(db, "checkout")
+
+  const tableCheckoutHeader = [
+    '',
+    'PRODUTO',
+    'QTD',
+    'PREÇO',
+    'Sub-total',
+    'Remover Item',
+  ];
+
+  useEffect(() => {
+    const total = cart.reduce((acc, curr) => acc + Number(curr.price * curr.quantity), 0).toFixed(2);
+    setCartTotalPrice(total);
+  }, [cart])
+  
+
+  const checkout = async () => {
+    const saveCheckoutDb  = await cart.map(async (eachCart) => {
+      const savedWithouImage = {
+        brand: await eachCart.brand,
+        description: await eachCart.description,
+        price: await eachCart.price,
+        quantity: await eachCart.quantity,
+        total: cartTotalPrice,
+      }
+      const savedInDB = await addDoc(shoppingCartRef, savedWithouImage)
+      if(savedInDB) {
+        setMessage(true)
+        setCartTotalPrice(0)
+        setCart([])
+        setTimeout(() => navegate('/'), 1002) 
+        setTimeout(() => setMessage(false), 1000) 
+        return saveCheckoutDb
+      }
+    })
+  }
+
   return (
-    <div>ShoppingCart</div>
+    <>
+    <div className='message'>{(message && 'Pedido finalizado com sucesso')}</div>
+      <table className='tableCheckout'>
+        {/* <caption>Finalizar pedido</caption> */}
+        <thead className='theadTitle'>
+          <tr>
+            {
+              tableCheckoutHeader.map((title, index) => (
+                <th key={ index }>{title} </th>
+              ))
+            }
+          </tr>
+        </thead>
+
+        <tbody>
+          {
+            cart.map((eachCart) => (
+              <tr key={eachCart.id}>
+                <td>{<img  className='eachImageShopping' src={eachCart.urlImage} alt={eachCart.description}/>}</td>
+                <td>{eachCart.brand} - {eachCart.description}</td>
+                <td>{eachCart.quantity}</td>
+                <td>{eachCart.price}</td>
+                <td>{(eachCart.price * eachCart.quantity).toFixed(2)}</td>
+                <td>
+                  <button className='buttonRemove'
+                    type="button"
+                    onClick={ () => removeProduct({ id: eachCart.id }) }
+                  >
+                    X
+                  </button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      <div className='totalPrice'>
+              {`Total: ${cartTotalPrice}` }
+        
+        <button
+          className='buttonCheckout'
+          type="button"
+          onClick={ () => checkout()}
+          >
+            Finalizar Pedido
+        </button>
+        </div>
+    </>
   )
 }
 
