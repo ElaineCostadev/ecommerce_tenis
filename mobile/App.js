e19e7b27475feb42a2abdc44fac0acb73398b827
elainecostadev <elainerscosta@hotmail.com>
2023-03-22 21:19:57 -0300

    Feat: App fazendo as rotas com navigation e Stack.Screen

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ProductsProviderApp } from './src/context/ProductsContextApp';
+import Home from './src/screens/Home';
+import ShoppingCard from './src/screens/ShoppingCard';
+import Header from './src/components/Header';
+import { StyleSheet } from 'react-native';
 
-export default function App() {
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer sytle={styles.container}> 
+        <Stack.Navigator initialRouteName='Home'
+          screenOptions={ { backgroundColor: '#ffffff' }}
+        >
+            <Stack.Screen
+              name='Home'
+              component={Home}
+              options={{
+                headerTitle: () => <Header />,
+                // headerTitle: () => <Header />,
+                // headerTransparent: true,
+                // headerBackVisible: false,
+                // headerLeft: 
+
+                // headerTransparent: true,
+                // headerStyle: { backgroundColor: '#141419' },
+                // headerTintColor: '#fff',
+                // headerTitleStyle: { fontWeight: 'bold' },
+              }}
+              
+            />
+
+            <Stack.Screen 
+              name='ShoppingCard'
+              component={ShoppingCard}
+              options={{
+                // hideNavigationBar: false,
+                headerTitle: () => <Header />,
+                // headerTitle: { backgroundColor: '#313131' },
+
+              }}
+            />
+        </Stack.Navigator>
+      </NavigationContainer>
+    
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    // margin: 0,
+  }
+
+})
+
+export default () => {
+  return (
+    <ProductsProviderApp>
+      <App />
+    </ProductsProviderApp>
+  )
+}
