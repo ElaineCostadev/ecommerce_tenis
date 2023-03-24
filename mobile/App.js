564c1936e6b343d2541fd20c872182424aa6d74c
elainecostadev <elainerscosta@hotmail.com>
2023-03-24 13:58:45 -0300

    fix: ajustando App

--- mobile/App.js
+++ mobile/App.js
@@ -5,8 +5,6 @@ import { ProductsProviderApp } from './src/context/ProductsContextApp';
 import Home from './src/screens/Home';
 import ShoppingCard from './src/screens/ShoppingCard';
 import Header from './src/components/Header';
-import { StyleSheet } from 'react-native';
-
 
 const Stack = createNativeStackNavigator();
 
@@ -14,50 +12,26 @@ function App() {
   return (
     <NavigationContainer sytle={styles.container}> 
         <Stack.Navigator initialRouteName='Home'
-          screenOptions={ { backgroundColor: '#ffffff' }}
         >
             <Stack.Screen
               name='Home'
               component={Home}
               options={{
                 headerTitle: () => <Header />,
-                // headerTitle: () => <Header />,
-                // headerTransparent: true,
-                // headerBackVisible: false,
-                // headerLeft: 
-
-                // headerTransparent: true,
-                // headerStyle: { backgroundColor: '#141419' },
-                // headerTintColor: '#fff',
-                // headerTitleStyle: { fontWeight: 'bold' },
               }}
-              
             />
 
             <Stack.Screen 
               name='ShoppingCard'
               component={ShoppingCard}
               options={{
-                // hideNavigationBar: false,
                 headerTitle: () => <Header />,
-                // headerTitle: { backgroundColor: '#313131' },
-
               }}
             />
         </Stack.Navigator>
       </NavigationContainer>
-    
-
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // margin: 0,
-  }
-
-})
 
 export default () => {
   return (
